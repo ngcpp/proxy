63f2b909f1f0d2fe9c357ec36bb3c8a43c92411f
Mingxin Wang <mingxwa@outlook.com>
2026-03-19 17:25:47 +0800

    Update proxiable_target

--- include/proxy/v4/proxy.h
+++ include/proxy/v4/proxy.h
@@ -1784,8 +1784,7 @@ concept inplace_proxiable_target = proxiable<details::inplace_ptr<T>, F>;
 
 template <class T, class F>
 concept proxiable_target =
-    proxiable<details::observer_ptr<T&, const T&, T&&, const T&&>,
-              observer_facade<F>>;
+    proxiable<details::observer_ptr<T&, const T&, T&&, const T&&>, F>;
 
 template <class T>
   requires(is_bitwise_trivially_relocatable_v<T>)
